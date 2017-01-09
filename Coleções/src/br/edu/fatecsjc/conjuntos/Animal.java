c5c3a08376d1a439b3269d0e6c16f8baae380400
abraaohenriquevasconcelos <henrique.vscncls@gmail.com>
2017-01-09 10:12:54 -0200

    ...

--- Coleções/src/br/edu/fatecsjc/conjuntos/Animal.java
+++ Coleções/src/br/edu/fatecsjc/conjuntos/Animal.java
@@ -1,8 +1,99 @@
 package br.edu.fatecsjc.conjuntos;
 
-public class Animal {
+public class Animal implements Comparable<Animal>{
 	private String nome;
 	private String especie;
+	private String apelido;
 	
+	
+	
+	
+	public Animal(String nome, String especie, String apelido) {
+		this.nome = nome;
+		this.especie = especie;
+		this.apelido = apelido;
+	}
+
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((especie == null) ? 0 : especie.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	
+	
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Animal other = (Animal) obj;
+		if (especie == null) {
+			if (other.especie != null)
+				return false;
+		} else if (!especie.equals(other.especie))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+
+	public String getEspecie() {
+		return especie;
+	}
+
+
+
+
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+	
+	
+	public String getApelido() {
+		return apelido;
+	}
+
+
+
+
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+	
+	@Override
+	public int compareTo(Animal outroAnimal){
+		return this.especie.compareTo(outroAnimal.getEspecie());
+	}
 	
 }
--- Coleções/src/br/edu/fatecsjc/conjuntos/Hash_Set.java
+++ Coleções/src/br/edu/fatecsjc/conjuntos/Hash_Set.java
@@ -1,6 +1,7 @@
 package br.edu.fatecsjc.conjuntos;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class Hash_Set {
@@ -10,12 +11,36 @@ public class Hash_Set {
 		//2)Set não permite elementos duplicados
 		//3)Um "Set" não é indexado e por isso não tem o método "get" das listas
 		//4)HashSet não tem ordem
-		//5)Um "Set" uso o método equals pra verificar se o elemento já existe
+		//5)Um "Set" uso o método equals pra verificar se o elemento já existe no conjunto
 		//6)LinkedHashSet mantém a ordem de inserção dos elementos
-		Set<String> conjuntoDePalavras = new HashSet<>();
-		conjuntoDePalavras.add("jumento");
-		conjuntoDePalavras.add("besta-fera");
-
+		
+		Animal jumento = new Animal("Jumento", "cavalos", "burro de carga");
+		Animal galinha = new Animal("Galinha", "voadores", "sem charme");
+		Animal jumentu = new Animal ("Jumento", "cavalos", "não vale nada");//Não vai ser adicionado porque de acordo com o equals já existe um jumento na coleção
+		
+		Set<Animal> animais = new HashSet<>();
+		
+		animais.add(jumento);
+		animais.add(galinha);
+		animais.add(jumentu);
+		
+		for(Animal animal : animais){
+			//System.out.println("Nome: "+animal.getNome()+" - Espécie: "+animal.getEspecie()+" - Apelido: "+animal.getApelido());
+		}
+		
+		/*
+		 * ********************************************************************************
+		 * ********************************************************************************
+		 */
+		
+		Set<Animal> animais_ordenados = new LinkedHashSet<>();
+		animais_ordenados.add(jumentu);
+		animais_ordenados.add(jumento);
+		animais_ordenados.add(galinha);
+		
+		for(Animal animal : animais_ordenados){
+			System.out.println("Nome: "+animal.getNome()+" - Espécie: "+animal.getEspecie()+" - Apelido: "+animal.getApelido());
+		}
 	}
 
 }
